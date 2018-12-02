818510b3c1f6f62592b47867abdc2b4ec7f3b4f3
Yuto Nakano <yuto.44.bb@gmail.com>
2018-12-02 09:22:10 +0900

    更新

--- Scraping.py
+++ Scraping.py
@@ -1,18 +1,35 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+
+import time
+import json
 
 
 def search(place_name):
+    options = Options()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
 
-    driver = webdriver.Chrome()
     driver.get("https://tabelog.com/")
+    WebDriverWait(driver, 10).until(
+        EC.title_contains("食べログ")
+    )
+
     input_elem = driver.find_element_by_name("sa")
     input_elem.send_keys(place_name)
+
     input_elem.click()
 
     elem_search_word = driver.find_element_by_name("sk")
+
     elem_search_word.send_keys("オムライス")
+
     elem_search_btn = driver.find_element_by_id("js-global-search-btn")
+
     elem_search_btn.click()
     page_source = driver.page_source
 
@@ -26,16 +43,22 @@ def extraction(page_source):
 
     for item_list in div_item_list[0].select("div.list-rst__wrap"):
         img_tags = item_list.select("img.js-cassette-img")
+
         img = img_tags[0].get("data-original")
         omulette_img.append(img)
-        item_name_tags = item_list.select("a.list-rst__rst-name-target")
-        i = item_name_tags[0].get("href")
+        item_restaurant_tags = item_list.select("a.list-rst__rst-name-target")
+        i = item_restaurant_tags[0].get("href")
         omulette_restaurant.append(i)
 
-    return omulette_restaurant, omulette_img
+        name = img_tags[0].find_all('a')
+
+        restaurant_name = name.text
+
+        omulette_name.append(restaurant_name)
+
+    return omulette_restaurant, omulette_img, omulette_name
 
 
 omulette_restaurant = []
+omulette_name = []
 omulette_img = []
-
-location = input()
--- omelette.py
+++ omelette.py
@@ -1,24 +1,51 @@
+from flask import Flask, render_template, request, redirect
 import Scraping
-from flask import Flask, render_template, request
+import time
+import json
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 
 app = Flask(__name__)
-Scraping.extraction(Scraping.search(Scraping.location))
 
 
-restaurants = Scraping.omulette_restaurant
-images = Scraping.omulette_img
+@app.route("/", methods=['POST', 'GET'])
+def send_text():
+    return render_template('index.html')
 
 
-@app.route("/sendtext", methods=['POST'])
-def sendtext():
-    return render_template('index.html', title=request.form['message'])
-
-
-@app.route("/")
+@app.route("/display", methods=['POST', 'GET'])
 def index():
     s = "レストラン"
+    location = request.form['location']
+    Scraping.extraction(Scraping.search(location))
 
-    return render_template('index.html', s=s, restaurants=restaurants, images=images)
+    dic = dict(zip(Scraping.omulette_restaurant,
+                   Scraping.omulette_img, Scraping.omulette_name))
+    restaurants = dic.items()
+
+    return render_template('index.html', s=s, restaurants=restaurants)
 
 
-app.run()
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0')
+
+
+'''
+    f = open('main.json', 'w')
+
+    json.dump(dic, f)
+    f.close()
+
+    f = open('main.json', 'r')
+
+    jsonData = json.load(f)
+
+    restaurants = jsonData.keys()
+    images = jsonData.values()
+    f.close()
+    print(jsonData)
+'''
