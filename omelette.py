0ce05813a812a5e7dbc4cb238f8e91336106cb52
Yuto Nakano <yuto.44.bb@gmail.com>
2018-12-12 22:22:46 +0900

    Use arrays instead of while loops

--- omelette.py
+++ omelette.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, request, redirect
-import Scraping
-import time
-import json
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+#coding: UTF-8
+from jinja2 import Template, Environment, FileSystemLoader
+from jsonData import writeData
+from Scraping import search
+from Scraping import extraction
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+import Scraping
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -17,35 +19,15 @@ def send_text():
 
 
 @app.route("/display", methods=['POST', 'GET'])
+@app.route("/display", methods=['POST', 'GET'])
 def index():
     s = "レストラン"
     location = request.form['location']
-    Scraping.extraction(Scraping.search(location))
-
-    dic = dict(zip(Scraping.omulette_restaurant,
-                   Scraping.omulette_img, Scraping.omulette_name))
-    restaurants = dic.items()
-
-    return render_template('index.html', s=s, restaurants=restaurants)
+    dic_result = writeData(Scraping.extraction(search(location)))
+    print(dic_result)
+    return render_template('index.html', s=s, restaurants=dic_result)
 
 
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0')
-
-
-'''
-    f = open('main.json', 'w')
-
-    json.dump(dic, f)
-    f.close()
-
-    f = open('main.json', 'r')
-
-    jsonData = json.load(f)
-
-    restaurants = jsonData.keys()
-    images = jsonData.values()
-    f.close()
-    print(jsonData)
-'''
